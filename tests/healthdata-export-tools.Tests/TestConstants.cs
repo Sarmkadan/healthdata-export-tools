a5dc069ae0144030688bb494edda329ae98742a3
Vladyslav Zaiets <rutova2@gmail.com>
2023-06-20 18:26:26 +0300

    Wip

---
+++ tests/healthdata-export-tools.Tests/TestConstants.cs
@@ -0,0 +1,16 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace HealthDataExportTools.Tests
+{
+    public static class TestConstants
+    {
+        public const string AuthorHeader = @"// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+";
+    }
+}
