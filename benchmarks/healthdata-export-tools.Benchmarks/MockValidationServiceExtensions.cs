b7e02a9098475ae7ee189d3b7c892619373edfac
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-02 13:38:11 +0200

    Handy extensions for MockValidation

--- benchmarks/healthdata-export-tools.Benchmarks/MockValidationServiceExtensions.cs
+++ benchmarks/healthdata-export-tools.Benchmarks/MockValidationServiceExtensions.cs
@@ -1,54 +1,128 @@
 using System;
 using System.Collections.Generic;
+using HealthDataExportTools.Domain.Models;
+using HealthDataExportTools.Services;
 
 namespace HealthDataExportTools.Benchmarks
 {
-    public static class MockValidationServiceExtensions
-    {
-        public static bool IsValid(this MockValidationService service, IEnumerable<ValidationResult> results)
-        {
-            foreach (var result in results)
-            {
-                if (result.HasErrors)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+	/// <summary>
+	/// Extension methods for <see cref="MockValidationService"/> that provide validation operations for benchmark scenarios.
+	/// </summary>
+	public static class MockValidationServiceExtensions
+	{
+		/// <summary>
+		/// Determines whether all validation results are valid.
+		/// </summary>
+		/// <param name="service">The validation service instance.</param>
+		/// <param name="results">The collection of validation results to check.</param>
+		/// <returns><see langword="true"/> if all results are valid; otherwise, <see langword="false"/>.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="results"/> is <see langword="null"/>.</exception>
+		public static bool IsValid(this MockValidationService service, IEnumerable<ValidationResult> results)
+		{
+			ArgumentNullException.ThrowIfNull(service);
+			ArgumentNullException.ThrowIfNull(results);
 
-        public static void ValidateAll(this MockValidationService service, 
-            SleepData sleepData, 
-            HeartRateData heartRateData, 
-            SpO2Data spO2Data, 
-            StepsData stepsData, 
-            ActivityData activityData)
-        {
-            var sleepResult = service.ValidateSleepData(sleepData);
-            var heartRateResult = service.ValidateHeartRateData(heartRateData);
-            var spO2Result = service.ValidateSpO2Data(spO2Data);
-            var stepsResult = service.ValidateStepsData(stepsData);
-            var activityResult = service.ValidateActivityData(activityData);
+			foreach (var result in results)
+			{
+				if (!result.IsValid)
+				{
+					return false;
+				}
+			}
 
-            // You can add error handling or logging here
-        }
+			return true;
+		}
 
-        public static Dictionary<string, ValidationResult> ValidateAll(this MockValidationService service, 
-            SleepData sleepData, 
-            HeartRateData heartRateData, 
-            SpO2Data spO2Data, 
-            StepsData stepsData, 
-            ActivityData activityData)
-        {
-            var results = new Dictionary<string, ValidationResult>
-            {
-                ["Sleep"] = service.ValidateSleepData(sleepData),
-                ["HeartRate"] = service.ValidateHeartRateData(heartRateData),
-                ["SpO2"] = service.ValidateSpO2Data(spO2Data),
-                ["Steps"] = service.ValidateStepsData(stepsData),
-                ["Activity"] = service.ValidateActivityData(activityData),
-            };
-            return results;
-        }
-    }
+		/// <summary>
+		/// Validates all provided health data records and returns a dictionary of validation results.
+		/// </summary>
+		/// <param name="service">The validation service instance.</param>
+		/// <param name="sleepData">The sleep data to validate.</param>
+		/// <param name="heartRateData">The heart rate data to validate.</param>
+		/// <param name="spO2Data">The SpO2 data to validate.</param>
+		/// <param name="stepsData">The steps data to validate.</param>
+		/// <param name="activityData">The activity data to validate.</param>
+		/// <returns>A dictionary mapping data type names to their validation results.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="service"/> is <see langword="null"/>.</exception>
+		public static Dictionary<string, ValidationResult> ValidateAll(
+			this MockValidationService service,
+			SleepData sleepData,
+			HeartRateData heartRateData,
+			SpO2Data spO2Data,
+			StepsData stepsData,
+			ActivityData activityData)
+		{
+			ArgumentNullException.ThrowIfNull(service);
+			ArgumentNullException.ThrowIfNull(sleepData);
+			ArgumentNullException.ThrowIfNull(heartRateData);
+			ArgumentNullException.ThrowIfNull(spO2Data);
+			ArgumentNullException.ThrowIfNull(stepsData);
+			ArgumentNullException.ThrowIfNull(activityData);
+
+			return new Dictionary<string, ValidationResult>(StringComparer.Ordinal)
+			{
+				["Sleep"] = service.ValidateSleepData(sleepData),
+				["HeartRate"] = service.ValidateHeartRateData(heartRateData),
+				["SpO2"] = service.ValidateSpO2Data(spO2Data),
+				["Steps"] = service.ValidateStepsData(stepsData),
+				["Activity"] = service.ValidateActivityData(activityData),
+			};
+		}
+
+		/// <summary>
+		/// Validates all provided health data records and returns a dictionary of validation results.
+		/// </summary>
+		/// <param name="service">The validation service instance.</param>
+		/// <param name="sleepData">The sleep data to validate.</param>
+		/// <param name="heartRateData">The heart rate data to validate.</param>
+		/// <param name="spO2Data">The SpO2 data to validate.</param>
+		/// <param name="stepsData">The steps data to validate.</param>
+		/// <param name="activityData">The activity data to validate.</param>
+		/// <returns>A dictionary mapping data type names to their validation results.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="service"/> is <see langword="null"/>.</exception>
+		public static Dictionary<string, ValidationResult> ValidateAll(
+			this MockValidationService service,
+			IEnumerable<SleepData> sleepData,
+			IEnumerable<HeartRateData> heartRateData,
+			IEnumerable<SpO2Data> spO2Data,
+			IEnumerable<StepsData> stepsData,
+			IEnumerable<ActivityData> activityData)
+		{
+			ArgumentNullException.ThrowIfNull(service);
+			ArgumentNullException.ThrowIfNull(sleepData);
+			ArgumentNullException.ThrowIfNull(heartRateData);
+			ArgumentNullException.ThrowIfNull(spO2Data);
+			ArgumentNullException.ThrowIfNull(stepsData);
+			ArgumentNullException.ThrowIfNull(activityData);
+
+			var results = new Dictionary<string, ValidationResult>(StringComparer.Ordinal);
+
+			foreach (var data in sleepData)
+			{
+				results.Add($"Sleep_{data.RecordDate:yyyyMMdd}", service.ValidateSleepData(data));
+			}
+
+			foreach (var data in heartRateData)
+			{
+				results.Add($"HeartRate_{data.RecordDate:yyyyMMdd}", service.ValidateHeartRateData(data));
+			}
+
+			foreach (var data in spO2Data)
+			{
+				results.Add($"SpO2_{data.RecordDate:yyyyMMdd}", service.ValidateSpO2Data(data));
+			}
+
+			foreach (var data in stepsData)
+			{
+				results.Add($"Steps_{data.RecordDate:yyyyMMdd}", service.ValidateStepsData(data));
+			}
+
+			foreach (var data in activityData)
+			{
+				results.Add($"Activity_{data.RecordDate:yyyyMMdd}_{data.ActivityType}", service.ValidateActivityData(data));
+			}
+
+			return results;
+		}
+	}
 }
