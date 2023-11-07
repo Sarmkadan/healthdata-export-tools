5fa9cd334bfe41ab63b35148e29e8f3620b748d2
Vladyslav Zaiets <rutova2@gmail.com>
2023-11-07 15:20:49 +0200

    more BackgroundTaskScheduler extension methods

--- healthdata-export-tools/Tasks/BackgroundTaskSchedulerExtensions.cs
+++ healthdata-export-tools/Tasks/BackgroundTaskSchedulerExtensions.cs
@@ -3,28 +3,26 @@
 namespace HealthDataExportTools.Tasks;
 
 /// <summary>
-/// Extension methods for BackgroundTaskScheduler providing additional functionality
+/// Extension methods for <see cref="BackgroundTaskScheduler"/> providing additional functionality
+/// for scheduling, managing, and querying background tasks.
 /// </summary>
 public static class BackgroundTaskSchedulerExtensions
 {
     /// <summary>
-    /// Schedule a task to run once after a delay
+    /// Schedules a task to run once after a specified delay.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <param name="taskName">Name of the task</param>
-    /// <param name="action">Action to execute</param>
-    /// <param name="delay">Delay before execution</param>
-    /// <returns>Task ID</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <param name="taskName">Name of the task to schedule.</param>
+    /// <param name="action">The asynchronous action to execute.</param>
+    /// <param name="delay">Delay before the task should execute.</param>
+    /// <returns>The unique identifier for the scheduled task.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> or <paramref name="action"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="taskName"/> is <see langword="null"/> or empty, or <paramref name="delay"/> is negative.</exception>
     public static string ScheduleOnce(this BackgroundTaskScheduler scheduler, string taskName, Func<Task> action, TimeSpan delay)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
-        if (string.IsNullOrEmpty(taskName))
-            throw new ArgumentException("Task name cannot be empty", nameof(taskName));
-
-        if (action == null)
-            throw new ArgumentNullException(nameof(action));
+        ArgumentNullException.ThrowIfNull(scheduler);
+        ArgumentNullException.ThrowIfNull(action);
+        ArgumentException.ThrowIfNullOrEmpty(taskName);
 
         if (delay.TotalMilliseconds < 0)
             throw new ArgumentException("Delay must be non-negative", nameof(delay));
@@ -34,24 +32,21 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Schedule a recurring task with a delay before the first execution
+    /// Schedules a recurring task with an initial delay before the first execution.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <param name="taskName">Name of the task</param>
-    /// <param name="action">Action to execute</param>
-    /// <param name="interval">Interval between executions</param>
-    /// <param name="initialDelay">Optional initial delay before first execution</param>
-    /// <returns>Task ID</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <param name="taskName">Name of the task to schedule.</param>
+    /// <param name="action">The asynchronous action to execute.</param>
+    /// <param name="interval">Interval between executions.</param>
+    /// <param name="initialDelay">Initial delay before the first execution. If not specified, uses the interval value.</param>
+    /// <returns>The unique identifier for the scheduled task.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> or <paramref name="action"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="taskName"/> is <see langword="null"/> or empty, or <paramref name="interval"/> is not positive.</exception>
     public static string ScheduleRecurring(this BackgroundTaskScheduler scheduler, string taskName, Func<Task> action, TimeSpan interval, TimeSpan initialDelay)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
-        if (string.IsNullOrEmpty(taskName))
-            throw new ArgumentException("Task name cannot be empty", nameof(taskName));
-
-        if (action == null)
-            throw new ArgumentNullException(nameof(action));
+        ArgumentNullException.ThrowIfNull(scheduler);
+        ArgumentNullException.ThrowIfNull(action);
+        ArgumentException.ThrowIfNullOrEmpty(taskName);
 
         if (interval.TotalMilliseconds <= 0)
             throw new ArgumentException("Interval must be positive", nameof(interval));
@@ -63,18 +58,17 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Cancel all tasks matching a specific name pattern
+    /// Cancels all tasks matching a specific name pattern.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <param name="namePattern">Pattern to match task names (supports * wildcard)</param>
-    /// <returns>Number of tasks cancelled</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <param name="namePattern">Pattern to match task names (supports * wildcard).</param>
+    /// <returns>Number of tasks cancelled.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="namePattern"/> is <see langword="null"/> or empty.</exception>
     public static int CancelTasksByName(this BackgroundTaskScheduler scheduler, string namePattern)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
-        if (string.IsNullOrEmpty(namePattern))
-            throw new ArgumentException("Name pattern cannot be empty", nameof(namePattern));
+        ArgumentNullException.ThrowIfNull(scheduler);
+        ArgumentException.ThrowIfNullOrEmpty(namePattern);
 
         var wildcardPattern = namePattern.Replace("*", ".*");
         var regex = new System.Text.RegularExpressions.Regex(wildcardPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
@@ -97,14 +91,14 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Get all pending tasks (tasks that haven't been executed yet)
+    /// Gets all pending tasks (tasks that haven't been executed yet).
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>List of pending tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>List of pending tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static List<ScheduledTask> GetPendingTasks(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
+        ArgumentNullException.ThrowIfNull(scheduler);
 
         return scheduler.GetScheduledTasks()
             .Where(t => t.IsPending)
@@ -112,14 +106,14 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Get all running tasks (tasks currently being executed)
+    /// Gets all running tasks (tasks currently being executed).
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>List of running tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>List of running tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static List<ScheduledTask> GetRunningTasks(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
+        ArgumentNullException.ThrowIfNull(scheduler);
 
         return scheduler.GetScheduledTasks()
             .Where(t => t.IsRunning)
@@ -127,14 +121,14 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Get all completed tasks
+    /// Gets all completed tasks.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>List of completed tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>List of completed tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static List<ScheduledTask> GetCompletedTasks(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
+        ArgumentNullException.ThrowIfNull(scheduler);
 
         return scheduler.GetScheduledTasks()
             .Where(t => t.IsCompleted)
@@ -142,86 +136,80 @@ public static class BackgroundTaskSchedulerExtensions
     }
 
     /// <summary>
-    /// Find a task by name
+    /// Finds a task by name.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <param name="taskName">Name of the task to find</param>
-    /// <returns>Found task or null</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <param name="taskName">Name of the task to find.</param>
+    /// <returns>Found task or null if not found.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="taskName"/> is <see langword="null"/> or empty.</exception>
     public static ScheduledTask? GetTaskByName(this BackgroundTaskScheduler scheduler, string taskName)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
-        if (string.IsNullOrEmpty(taskName))
-            throw new ArgumentException("Task name cannot be empty", nameof(taskName));
+        ArgumentNullException.ThrowIfNull(scheduler);
+        ArgumentException.ThrowIfNullOrEmpty(taskName);
 
         return scheduler.GetScheduledTasks()
             .FirstOrDefault(t => string.Equals(t.Name, taskName, StringComparison.OrdinalIgnoreCase));
     }
 
     /// <summary>
-    /// Check if a task with the given name exists
+    /// Checks if a task with the given name exists.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <param name="taskName">Name of the task to check</param>
-    /// <returns>True if task exists, false otherwise</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <param name="taskName">Name of the task to check.</param>
+    /// <returns>True if task exists, false otherwise.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="taskName"/> is <see langword="null"/> or empty.</exception>
     public static bool TaskExists(this BackgroundTaskScheduler scheduler, string taskName)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
         return scheduler.GetTaskByName(taskName) != null;
     }
 
     /// <summary>
-    /// Get the count of scheduled tasks
+    /// Gets the count of scheduled tasks.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>Number of scheduled tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>Number of scheduled tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static int GetTaskCount(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
+        ArgumentNullException.ThrowIfNull(scheduler);
         return scheduler.GetScheduledTasks().Count;
     }
 
     /// <summary>
-    /// Get the count of pending tasks
+    /// Gets the count of pending tasks.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>Number of pending tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>Number of pending tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static int GetPendingTaskCount(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
+        ArgumentNullException.ThrowIfNull(scheduler);
         return scheduler.GetPendingTasks().Count;
     }
 
     /// <summary>
-    /// Get the count of running tasks
+    /// Gets the count of running tasks.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>Number of running tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>Number of running tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static int GetRunningTaskCount(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
+        ArgumentNullException.ThrowIfNull(scheduler);
         return scheduler.GetRunningTasks().Count;
     }
 
     /// <summary>
-    /// Get the count of completed tasks
+    /// Gets the count of completed tasks.
     /// </summary>
-    /// <param name="scheduler">The task scheduler instance</param>
-    /// <returns>Number of completed tasks</returns>
+    /// <param name="scheduler">The task scheduler instance.</param>
+    /// <returns>Number of completed tasks.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="scheduler"/> is <see langword="null"/>.</exception>
     public static int GetCompletedTaskCount(this BackgroundTaskScheduler scheduler)
     {
-        if (scheduler == null)
-            throw new ArgumentNullException(nameof(scheduler));
-
+        ArgumentNullException.ThrowIfNull(scheduler);
         return scheduler.GetCompletedTasks().Count;
     }
 }
