fa8fe9e5c0dd13b74a2066a74e481addaa1a0901
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-11 20:52:53 +0000

    docs(healthdata-export-tools.tests): document BaseTest public API

--- tests/healthdata-export-tools.Tests/BaseTest.cs
+++ tests/healthdata-export-tools.Tests/BaseTest.cs
@@ -9,20 +9,39 @@ using Xunit;
 
 namespace HealthDataExportTools.Tests
 {
+    /// <summary>
+    /// Base class for tests that requires a test fixture.
+    /// </summary>
     public abstract class BaseTest : IClassFixture<TestFixture>
     {
+        /// <summary>
+        /// Gets the service provider for the test.
+        /// </summary>
         protected readonly ServiceProvider ServiceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseTest"/> class.
+        /// </summary>
+        /// <param name="fixture">The test fixture.</param>
         protected BaseTest(TestFixture fixture)
         {
             ServiceProvider = fixture.ServiceProvider;
         }
     }
 
+    /// <summary>
+    /// Test fixture class that provides a service provider.
+    /// </summary>
     public sealed class TestFixture
     {
+        /// <summary>
+        /// Gets the service provider.
+        /// </summary>
         public ServiceProvider ServiceProvider { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestFixture"/> class.
+        /// </summary>
         public TestFixture()
         {
             var services = new ServiceCollection();
