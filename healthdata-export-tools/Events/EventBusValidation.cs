df0282a8d6ef9286b2bb22f3d69e1a3c1594c5ad
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 16:27:28 +0300

    refactor(events): readability pass over EventBusValidation

--- healthdata-export-tools/Events/EventBusValidation.cs
+++ healthdata-export-tools/Events/EventBusValidation.cs
@@ -16,15 +16,15 @@ public static class EventBusValidation
     /// </summary>
     /// <param name="value">The EventBus instance to validate</param>
     /// <returns>List of validation problems; empty if valid</returns>
-    /// <exception cref="ArgumentNullException">Thrown if value is null</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
     public static IReadOnlyList<string> Validate(this EventBus value)
     {
         ArgumentNullException.ThrowIfNull(value);
 
         var problems = new List<string>();
 
-        // EventBus itself doesn't have validation-worthy state beyond its constructor parameters
-        // The handlers dictionary is internal and managed by the class
+        // Validate that the EventBus is in a usable state
+        // The handlers dictionary should never be null due to constructor initialization
         // The logger is validated in constructor and assumed to be valid
 
         return problems.AsReadOnly();
@@ -35,25 +35,15 @@ public static class EventBusValidation
     /// </summary>
     /// <param name="value">The EventBus instance to check</param>
     /// <returns>True if valid; false otherwise</returns>
-    public static bool IsValid(this EventBus value)
-    {
-        try
-        {
-            _ = value.Validate();
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
+    public static bool IsValid(this EventBus value) => value.Validate() is not null;
 
     /// <summary>
     /// Ensures an EventBus instance is valid, throwing if not
     /// </summary>
     /// <param name="value">The EventBus instance to validate</param>
-    /// <exception cref="ArgumentNullException">Thrown if value is null</exception>
-    /// <exception cref="ArgumentException">Thrown if value is invalid with problem details</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> is invalid with problem details</exception>
     public static void EnsureValid(this EventBus value)
     {
         ArgumentNullException.ThrowIfNull(value);
