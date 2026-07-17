b4ef131d4d1e980e051e57843194d09a3bdb36c0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:22:35 +0300

    small refactor - ExportServiceTestsExtensions

--- tests/healthdata-export-tools.Tests/ExportServiceTestsExtensions.cs
+++ tests/healthdata-export-tools.Tests/ExportServiceTestsExtensions.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 namespace HealthDataExportTools.Tests
 {
     /// <summary>
-    /// Provides extension methods for the <see cref="ExportServiceTests"/> class.
+    /// Provides extension methods for discovering and analyzing test methods in the <see cref="ExportServiceTests"/> class.
     /// </summary>
     public static class ExportServiceTestsExtensions
     {
         /// <summary>
         /// Retrieves the names of all test methods in the <see cref="ExportServiceTests"/> instance.
+        /// A test method is defined as any public instance method whose name starts with "Test".
         /// </summary>
         /// <param name="tests">The <see cref="ExportServiceTests"/> instance.</param>
         /// <returns>An <see cref="IReadOnlyList{T}"/> of test method names.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
         public static IReadOnlyList<string> GetTestMethodNames(this ExportServiceTests tests)
         {
             ArgumentNullException.ThrowIfNull(tests);
@@ -28,8 +29,9 @@ namespace HealthDataExportTools.Tests
         /// <param name="tests">The <see cref="ExportServiceTests"/> instance.</param>
         /// <param name="methodName">The name of the test method.</param>
         /// <returns>True if the test method is asynchronous; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> is null.</exception>
-        /// <exception cref="ArgumentException">Thrown if <paramref name="methodName"/> is null or empty.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="methodName"/> is <see langword="null"/> or empty.</exception>
+        /// <exception cref="MissingMethodException">The specified method does not exist on the type.</exception>
         public static bool IsTestMethodAsync(this ExportServiceTests tests, string methodName)
         {
             ArgumentNullException.ThrowIfNull(tests);
@@ -40,10 +42,11 @@ namespace HealthDataExportTools.Tests
 
         /// <summary>
         /// Retrieves the names of all asynchronous test methods in the <see cref="ExportServiceTests"/> instance.
+        /// A test method is defined as any public instance method whose name starts with "Test".
         /// </summary>
         /// <param name="tests">The <see cref="ExportServiceTests"/> instance.</param>
         /// <returns>An <see cref="IReadOnlyList{T}"/> of asynchronous test method names.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <see langword="null"/>.</exception>
         public static IReadOnlyList<string> GetAsyncTestMethodNames(this ExportServiceTests tests)
         {
             ArgumentNullException.ThrowIfNull(tests);
