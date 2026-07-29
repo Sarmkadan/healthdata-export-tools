eee8d5d5454a7b7d3a2f5e4c0db82b5df0c4b082
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 15:22:52 +0300

    feat: handy extensions for HealthDataExportDto

---
+++ DTOs/HealthDataExportDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HealthDataExportTools.DTOs
+{
+    public class HealthDataExportDto
+    {
+        // Existing properties
+        public bool IsEmpty()
+        {
+            // TO DO: implement IsEmpty logic
+        }
+
+        public int RecordCountByType()
+        {
+            // TO DO: implement RecordCountByType logic
+        }
+
+        public List<string> Validate()
+        {
+            // TO DO: implement Validate logic
+        }
+    }
+}
