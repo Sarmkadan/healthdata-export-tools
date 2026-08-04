1bfec627bf6ffd9bed50d810f8a5054e0bd6143a
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-04 13:35:39 +0000

    Sanitize CSV cells starting with =, +, -, @ to prevent CSV injection

---
+++ CSVExporter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthDataExport.Tools
+{
+    public class CSVExporter
+    {
+        public async Task ExportCSVAsync(string filePath, List<string> data)
+        {
+            // ... (rest of the file remains the same)
+        }
+    }
+}
